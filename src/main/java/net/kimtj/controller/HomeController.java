d639ef2a5c519127e440ba5e4e06ad2efe06ac6b
KIM TAE JUN <kaminori1999@gmail.com>
2023-01-06 15:41:36 +0900

    로그인 폼 1차 구현(GET->POST)

--- src/main/java/net/kimtj/controller/HomeController.java
+++ src/main/java/net/kimtj/controller/HomeController.java
@@ -2,17 +2,34 @@ package net.kimtj.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Date;
 
-@Controller
+@Controller 
 public class HomeController {
 	
-    @RequestMapping("home/index")
+    @RequestMapping("home/index") //URL이 요청된 경우에 이 액션 메소드가 실행 (GET, POST 무관)
     public String index(Model model) {
         model.addAttribute("message", "hello");
         model.addAttribute("now",new Date());
         return "home/index";
     }
-
+    
+    @GetMapping("home/login") //URL이 GET 방식으로 서버에 요청되면 실행
+    public String text1(Model model) {
+    	model.addAttribute("message", "로그인 화면입니다.");
+    	model.addAttribute("now",new Date());
+    	return "home/login";
+    }
+    
+    @PostMapping("home/login") //URL이 Post 방식으로 서버에 요청되면 실행
+    public String text2(Model model, String userid, String passwd) {
+    	model.addAttribute("message", "POST 출력 완료");
+    	model.addAttribute("now",new Date());
+    	System.out.println(userid);
+        System.out.println(passwd);
+        return "home/login";
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/home/login.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %> <!-- jstl확장태그를 위한 선언  -->
+
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+	<link href="${path}/resources/css/loginstyle.css" rel="stylesheet"/>
+	<style>
+		.login-box{
+			width:300px;
+        	height:300px;
+		}
+	</style>
+</head>
+<body>
+  <h3>${ message }</h3>
+  <h3><fmt:formatDate pattern = "yyyy-MM-dd HH:mm:ss" value = "${ now }"/></h3> <!-- 날짜와 시각 표현을 위한 jstl문  -->
+  <form method="POST">
+  		<div class = "login-box">
+            <div class="input-box">
+                <label for="username">아이디</label>
+                <input id="userid" type="text" name="userid" placeholder="아이디">
+            </div>
+
+            <div class="input-box">
+                <label for="password">비밀번호</label>
+                <input id="passwd" type="password" name="passwd" placeholder="비밀번호">
+            </div>
+            <div id="forgot">비밀번호 찾기</div>
+            <input type="submit" value="로그인">
+        </div>
+</form>
+</body>
+</html>
